59938a20d5b1bbd1f29be9f6c6cb007436ac8122
JX_Sun <sunkross106@gmail.com>
2018-04-07 00:27:50 +0800

    add gpu optins

--- train_ds.sh
+++ train_ds.sh
@@ -1,1 +1,1 @@
-python PreTrainDs.py ./traindata/style  ./traindata/trainDataOfIndex.npy no ./Model/Ds.pkl ./Model/Ds_emb.pkl 21
+CUDA_VISIBLE_DEVICES=$1 python PreTrainDs.py ./traindata/style  ./traindata/trainDataOfIndex.npy no ./Model/Ds.pkl ./Model/Ds_emb.pkl 41
